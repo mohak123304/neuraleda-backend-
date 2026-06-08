dedb08b678cfc6f81ce0ef5fda99a51545ed2500
mohak123304 <mohakjadhav20@gmail.com>
2026-06-08 08:23:05 +0000

    Add root and health routes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("NeuralEDA backend running!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
